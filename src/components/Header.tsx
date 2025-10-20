ede7028916bff4a796ab679db0311093e3bff691
Zarfishan Ali <zf.ali@hotmail.com>
2025-10-20 16:51:21 +0100

    added hosting website

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -45,6 +45,9 @@ const Header = () => {
   }, {
     label: "Contact",
     href: "#contact"
+  } , {
+    label: "Hosting",
+    href: "https://byte6hosting.caramelwebstudios.com" // external subdomain link
   }];
   return <header className="fixed top-0 left-0 right-0 z-50 bg-white/95 backdrop-blur-sm border-b border-border/50 shadow-soft">
       <div className="max-w-6xl mx-auto px-6">
